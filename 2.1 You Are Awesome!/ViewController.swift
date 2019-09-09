8c67c9d8be8897ad1115f56b5cdbef6afb267abe
Braden Hamlin <braden@placefull.com>
2019-09-09 12:27:15 -0700

    2.14 completed - using declare but not initialize

--- 2.1 You Are Awesome!/ViewController.swift
+++ 2.1 You Are Awesome!/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
                         "You are so Cool"]
         
         //var newIndex = -1
-        var newIndex: Int //declares but doesn't initialize newIndex 
+        var newIndex: Int //declares but doesn't initialize newIndex  
         
         repeat {
             newIndex = Int.random(in: 0..<messages.count)
